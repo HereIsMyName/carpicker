7cd155999684346716dac4988bb5bb50bee7f73f
Mike <43222520+mhopard@users.noreply.github.com>
2018-11-01 14:35:50 -0400

    Add files via upload

---
+++ src/carFacts.js
@@ -0,0 +1,119 @@
+module.exports = {
+  size: {
+      small: [
+          [
+              'coupe',
+              'hatchback',
+              'sedan',
+              'moto',
+              'exotic'
+          ],
+          [
+              'small'
+          ]
+      ],
+      midsize: [
+          [
+              'sedan',
+              'van',
+              'pickup',
+              'suv'
+          ],
+          [
+              'midsize'
+          ],
+      ],
+      large: [
+          [
+              'van',
+              'pickup',
+              'suv'
+          ],
+          [
+              'large'
+          ]
+      ]
+  },
+  usage: {
+      commute: [
+          [
+              'coupe',
+              'hatchback',
+              'sedan',
+              'pickup',
+              'moto'
+          ],
+          [
+              'commute'
+          ]
+      ],
+      leisure: [
+          [
+              'coupe',
+              'hatchback',
+              'sedan',
+              'pickup',
+              'suv',
+              'moto',
+              'exotic'
+          ],
+          [
+              'leisure'
+          ]
+      ],
+      family: [
+          [
+              'sedan',
+              'van',
+              'suv'
+          ],
+          [
+              'family'
+          ]
+      ],
+      utility: [
+          [
+              'van',
+              'pickup',
+              'suv'
+          ],
+          [
+              'utility'
+          ]
+      ],
+  },
+  fuel: {
+      economic: [
+          [
+              'coupe',
+              'hatchback',
+              'sedan',
+              'moto'
+          ],
+          [
+              'economic'
+          ],
+      ],
+      moderate: [
+          [
+              'sedan',
+              'pickup',
+              'van'
+          ],
+          [
+              'moderate'
+          ],
+      ],
+      power: [
+          [
+              'van',
+              'pickup',
+              'suv',
+              'exotic'
+          ],
+          [
+              'power'
+          ]
+      ]
+  }
+};
