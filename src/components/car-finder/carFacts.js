1676f0f0a7e00b7bdd970e882f0aed82a64e4fe7
Mike <cbookmh@gmail.com>
2019-01-16 16:58:35 -0500

    Many things

--- src/components/car-finder/carFacts.js
+++ src/components/car-finder/carFacts.js
@@ -1,89 +1,72 @@
 module.exports = {
-  size: {
-      small: [
-          [
-              'coupe',
-              'hatchback',
-              'sedan',
-              'moto',
-              'exotic'
-          ]
-      ],
-      midsize: [
-          [
-              'sedan',
-              'van',
-              'pickup',
-              'suv'
-          ]
-      ],
-      large: [
-          [
-              'van',
-              'pickup',
-              'suv'
-          ]
-      ]
-  },
-  usage: {
-      commute: [
-          [
-              'coupe',
-              'hatchback',
-              'sedan',
-              'pickup',
-              'moto'
-          ]
-      ],
-      leisure: [
-          [
-              'coupe',
-              'hatchback',
-              'sedan',
-              'pickup',
-              'suv',
-              'moto',
-              'exotic'
-          ]
-      ],
-      family: [
-          [
-              'sedan',
-              'van',
-              'suv'
-          ]
-      ],
-      utility: [
-          [
-              'van',
-              'pickup',
-              'suv'
-          ]
-      ],
-  },
-  fuel: {
-      economic: [
-          [
-              'coupe',
-              'hatchback',
-              'sedan',
-              'moto'
-          ]
-      ],
-      moderate: [
-          [
-              'sedan',
-              'pickup',
-              'van'
-          ]
-      ],
-      power: [
-          [
-              'van',
-              'pickup',
-              'suv',
-              'exotic'
-          ]
-      ]
-  }
+    size: {
+        small: [
+            'coupe',
+            'hatchback',
+            'sedan',
+            'motorcycle',
+            'exotic'
+        ],
+        midsize: [
+            'sedan',
+            'van',
+            'pickup',
+            'suv',
+            'exotic'
+        ],
+        large: [
+            'van',
+            'pickup',
+            'suv'
+        ]
+    },
+    usage: {
+        commute: [
+            'coupe',
+            'hatchback',
+            'sedan',
+            'pickup',
+            'motorcycle'
+        ],
+        leisure: [
+            'coupe',
+            'hatchback',
+            'sedan',
+            'pickup',
+            'suv',
+            'motorcycle',
+            'exotic'
+        ],
+        family: [
+            'sedan',
+            'van',
+            'suv'
+        ],
+        utility: [
+            'van',
+            'pickup',
+            'suv'
+        ],
+    },
+    fuel: {
+        economic: [
+            'coupe',
+            'hatchback',
+            'sedan',
+            'pickup',
+            'motorcycle'
+        ],
+        moderate: [
+            'sedan',
+            'pickup',
+            'van',
+            'suv'
+        ],
+        power: [
+            'van',
+            'pickup',
+            'suv',
+            'exotic'
+        ]
+    }
 };
